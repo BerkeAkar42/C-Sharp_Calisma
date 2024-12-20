e49ed8c30670a1ce4f8246b48d7aa9c22cc6fdba
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:45:11 +0300

    C#
    Kullanıcıdan iki sayı ve yapılacak işlem girdisi aldıktan sonra, dört işlem yapabilecek kapasiteli mini bir hesap makinesi. Geliştirmelere açıktır.

---
+++ MiniHesapMakinesi/MiniHesapMakinesi/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHesapMakinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Kullanıcıdan iki tane sayı alıp bu sayıları dört işlem ile hesaba dökebilen mini bir hesap makinesi programı */
+
+
+            Console.Write("Lütfen 1. Sayıyı Giriniz: ");
+            double s1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lütfen 2. Sayıyı Giriniz: ");
+            double s2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Toplama: +");
+            Console.WriteLine("Çıkarma: -");
+            Console.WriteLine("Çarpma: *"); 
+            Console.WriteLine("Bölme: /");
+
+            Console.WriteLine();
+
+            Console.Write("Yapmak İstediğiniz İşlemi Seçiniz: ");
+            string islem = Console.ReadLine().ToString();
+
+
+            islembolumu:
+            double sonuc = 0;
+
+            if (islem == "+")
+            {
+                sonuc = s1 + s2;
+            }
+            else if (islem == "-")
+            {
+                sonuc = s1 - s2;
+            }
+            else if (islem == "*")
+            {
+                sonuc = s1 * s2;
+            }
+            else if (islem == "/")
+            {
+                sonuc = s1 / s2;
+            }
+            else
+            {
+                Console.WriteLine("Yapmak İstediğiniz İşlemi Yanlış Girdiniz! Lütfen Tekrardan Deneyiniz.");
+                goto islembolumu;
+            }
+
+
+            Console.WriteLine("İşlemin Sonucu: " + sonuc.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
