ea41a5718b753f175d23b162334c2b4e0474025e
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:34:00 +0300

    C#
    Klavyeden alınan sayının faktöriyelini hesaplar. Geliştirmelere açıktır.

---
+++ FaktoriyelHesaplama/FaktoriyelHesaplama/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaktoriyelHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Klavyeden alınan sayının faktoriyelini hesaplayan program */
+
+
+            int sayac = 1;
+            int sonuc = 1;
+
+            Console.Write("Sayı Giriniz: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            while (sayac<=x)
+            {
+                sonuc *= sayac; //sonuc = sonuc * sayac;
+                sayac++;
+            }
+
+            Console.WriteLine("Sonuc: " + sonuc);
+            Console.ReadKey();
+
+        }
+    }
+}
