4cf86a64bd3dc47a1e98aaf68c26b7c564c66989
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:38:29 +0300

    C#
    Klavyeden üçgenin iki açısını aldıktan sonra üçüncü açısını ekrana yazdırır; üçgenin iç açıları toplamı kuralına da uyup uymadığını kontrol eder. Geliştirmelere açıktır.

---
+++ UcgeninUcuncuAcisiniBulma/UcgeninUcuncuAcisiniBulma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcgeninUcuncuAcisiniBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Klavyeden üçgenin iki açısını aldıktan sonra üçüncü açısını ekrana yazdıran; üçgenin iç açıları toplamı kuralına da uyup uymadığını kontrol eden program */
+
+            int top = 0;
+
+            Baslangic:
+            for (int sayac=2; sayac>0; sayac--)
+            {
+                Console.Write("Açıyı Giriniz: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                top += x;
+            }
+
+            if (top<180)
+            {
+                top = 180 - top;
+                Console.WriteLine("Üçgenin Üçüncü Açısı: " + top);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Üçgenin İç Açıları Kurallarına Uyan Değerler Girmediniz! Lütfen Tekrardan Giriniz.");
+                top = 0;
+                goto Baslangic;
+            }
+
+        }
+    }
+}
