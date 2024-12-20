406b3d9e56d062b166f6a76fee6f6bf4d3ae055a
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:31:03 +0300

    C#
    Kullanıcıdan dikdörtgenin kısa ve uzun kenarını alarak alan hesabı yapar. Geliştirmelere Açıktır.

---
+++ DikdortgenAlanHesabiYapanProgram/DikdortgenAlanHesabiYapanProgram/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DikdortgenAlanHesabiYapanProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Kullanıcıdan dikdörtgenin kısa ve uzun kenarı alınarak alan hesabı yapan program */
+
+            Kenar1Giris:
+            Console.Write("Kısa Kenarı Giriniz: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            if (x1>0)
+            {
+
+                Kenar2Giris:
+                Console.Write("Uzun Kenarı Giriniz: ");
+                int x2 = Convert.ToInt32(Console.ReadLine());
+
+                if (x2>0)
+                {
+
+                    int alan = x1 * x2;
+                    Console.WriteLine("Alan: " + alan);
+                    Console.ReadKey();
+
+                }
+                else
+                {
+                    Console.WriteLine("Girilen Sayı Bir Uzunluk Belirtmemektedir! Lütfen Tekrardan Giriniz.");
+                    goto Kenar2Giris;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Girilen Sayı Bir Uzunluk Belirtmemektedir! Lütfen Tekrardan Giriniz.");
+                goto Kenar1Giris;
+            }
+        }
+    }
+}
