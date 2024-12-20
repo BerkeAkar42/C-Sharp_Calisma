44c510d023b6ddff69751d2e35259b1a4aa35e94
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:32:51 +0300

    C#
    Klavyeden alınan sayının pozitif mi negatif mi olduğunu kontrol edip bunu ekrana yazdırır. Geliştirmelere açıktır.

---
+++ SayininPozitifNegatifOldugunuKontrolEdenProgram/SayininPozitifNegatifOldugunuKontrolEdenProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayininPozitifNegatifOldugunuKontrolEdenProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Klavyeden alınan sayının pozitif mi negatif mi olduğunu kontrol edip bunu ekrana yazdıran prgoram */
+
+
+            Console.Write("Sayı Giriniz: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x>0)
+            {
+                Console.WriteLine("Sayınız Pozitiftir.");
+            }
+            else if (x<0)
+            {
+                Console.WriteLine("Sayınız Negatiftir.");
+            }
+            else
+            {
+                Console.WriteLine("Sayınız Sıfırdır.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
