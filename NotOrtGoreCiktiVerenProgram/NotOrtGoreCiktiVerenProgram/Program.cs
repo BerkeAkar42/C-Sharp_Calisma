0ffb885c19608f64b043519ffdac15481263b695
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:29:46 +0300

    C#
    Kullanıcıdan iki not girilmesi istenir. Bu iki notun ortalaması alınarak bir kıyaslama yapılır. Ortalama 70 ve üzeri ise ekrana "İYİ" yazdırır. 70'den alt bir değer ise ekrana "KÖTÜ" yazdırır. Geliştirmelere Açıktır.

---
+++ NotOrtGoreCiktiVerenProgram/NotOrtGoreCiktiVerenProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotOrtGoreCiktiVerenProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Kullanıcıdan iki not girilmesi istenecek ve bu iki notun ortalaması alınarak belirlenen koşullara göre ekrana bir çıktı verecek.
+            Not ortalaması 70'den büyük ise ekrana "İYİ" yazacak. 70'den küçük ise ekrana "KÖTÜ" yazacack. */
+
+            int top = 0;
+            int sayac = 0;
+
+
+            while (sayac<2)
+            {
+                sayac++;
+                Console.Write("Lütfen Notunuzu Giriniz: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                top += x; //top= top + x;
+            }
+
+            int ort = top / sayac;
+
+            if (ort>=70)
+            {
+                Console.WriteLine("İYİ");
+            }
+            else
+            {
+                Console.WriteLine("KÖTÜ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
