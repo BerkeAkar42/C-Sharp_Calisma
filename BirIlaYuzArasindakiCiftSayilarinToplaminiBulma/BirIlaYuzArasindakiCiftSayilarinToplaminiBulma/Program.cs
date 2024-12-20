c6b388d59fd597de70352fa8ecd5fa9368af7490
Berke Akar <berkeakar4234@gmail.com>
2024-12-21 01:39:35 +0300

    C#
    1 ila 100 arasındaki çift sayıların toplamını bulup ekrana yazdırır. Geliştirmelere açıktır.

---
+++ BirIlaYuzArasindakiCiftSayilarinToplaminiBulma/BirIlaYuzArasindakiCiftSayilarinToplaminiBulma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirIlaYuzArasindakiCiftSayilarinToplaminiBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1 ila 100 arasındaki çift sayıların toplamını bulup ekrana yazdıran program */
+
+            int top = 0;
+
+            for (int sayac=1; sayac<101; sayac++)
+            {
+
+                if (sayac%2==0)
+                {
+                    top += sayac;
+                }
+
+            }
+
+            Console.WriteLine(top);
+            Console.ReadKey();
+
+        }
+    }
+}
